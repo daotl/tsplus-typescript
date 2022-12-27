af00091c0bb996287e8d66e4a41b36d28c3acc00
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-12-27 18:10:26 +0800

    build

--- lib/lib.tsplus.d.ts
+++ lib/lib.tsplus.d.ts
@@ -14,12 +14,29 @@ and limitations under the License.
 ***************************************************************************** */
 
 
-"use strict";
-var ts;
-(function (ts) {
-    var server;
-    (function (server) {
-        server.dynamicImport = (id) => import(id);
-    })(server = ts.server || (ts.server = {}));
-})(ts || (ts = {}));
-//# sourceMappingURL=dynamicImportCompat.js.map
+
+/// <reference no-default-lib="true"/>
+
+
+/// <reference lib="es5" />
+
+declare type PipeableShift<A extends any[]> = A extends [infer X, ...infer Rest] ? Rest : never
+declare type PipeableFirst<A extends any[]> = A extends [infer X, ...infer Rest] ? X : never
+
+/**
+ * @tsplus macro pipeable
+ */
+declare function Pipeable<F extends (self: any, ...rest: any) => any>(f: F): (...rest: PipeableShift<Parameters<F>>) => (self: PipeableFirst<Parameters<F>>) => ReturnType<F>
+
+/**
+ * @tsplus macro Derive
+ */
+declare function Derive<A>(explain?: "explain"): A
+
+/**
+ * @tsplus macro Do
+ */
+declare function Do<A>(f: (_: {
+    /** @tsplus macro Bind */
+    <X>(a: X): X
+}) => A): A
